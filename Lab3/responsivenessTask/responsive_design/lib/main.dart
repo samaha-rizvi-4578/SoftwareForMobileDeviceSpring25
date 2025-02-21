f133442e5019699808ba1e984f2abe3d54413a7d
SamahaRizvi <k214578@nu.edu.pk>
2025-02-21 15:19:25 +0500

    Implement responsive design with separate views for portrait and landscape modes

--- Lab3/responsivenessTask/responsive_design/lib/main.dart
+++ Lab3/responsivenessTask/responsive_design/lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'portrait_view.dart';
+import 'landscape_view.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,100 +12,28 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'TODO LIST',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const ResponsiveViewPage(),
+      home: const ToDoHomePage(),
     );
   }
 }
 
-class ResponsiveViewPage extends StatelessWidget {
-  const ResponsiveViewPage({super.key});
+class ToDoHomePage extends StatelessWidget {
+  const ToDoHomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    double screenWidth = MediaQuery.of(context).size.width;
-    double screenHeight = MediaQuery.of(context).size.height;
-    return screenWidth > screenHeight ? LandscapeWidget() : PortraitWidget();
-  }
-}
-
-class NumberItem extends StatelessWidget {
-  final int number;
-  const NumberItem({required this.number, super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return CircleAvatar(
-      backgroundColor: Colors.blue,
-      child: Text(
-        number.toString(),
-        style: const TextStyle(color: Colors.black, fontWeight: FontWeight.w900),
-      ),
-    );
-  }
-}
-
-class LandscapeWidget extends StatelessWidget {
-  LandscapeWidget({super.key});
-
-  final List<int> list1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-  final List<int> list2 = [11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
-
-  @override
-  Widget build(BuildContext context) {
+    bool isPortrait = MediaQuery.of(context).orientation == Orientation.portrait;
+    
     return Scaffold(
-      appBar: AppBar(title: const Text('Landscape View')),
-      body: FittedBox(
-        fit: BoxFit.contain, // Ensures content scales to fit within available space
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Expanded(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: _buildList(list1),
-              ),
-            ),
-            Expanded(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: _buildList(list2),
-              ),
-            ),
-          ],
-        ),
+      appBar: AppBar(
+        title: const Text('To Do'),
       ),
+      body: isPortrait ? const PortraitView() : const LandscapeView(),
     );
-  }
-
-  List<Widget> _buildList(List<int> list) {
-    return list.map((item) => NumberItem(number: item)).toList();
-  }
-}
-
-class PortraitWidget extends StatelessWidget {
-  PortraitWidget({super.key});
-
-  final List<int> list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('Portrait View')),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: _buildList(),
-        ),
-      ),
-    );
-  }
-
-  List<Widget> _buildList() {
-    return list.map((item) => NumberItem(number: item)).toList();
   }
 }
